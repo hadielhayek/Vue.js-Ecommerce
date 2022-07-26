e50ba15212f54a0e808b9e9fc8528ec9e673b426
Jesse <akohgodwinjesse@gmail.com>
2022-07-26 20:55:23 +0100

    movement movement

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
-     <img alt="Vue logo" src="./assets/logo.png" />
+   <Campaign/>
 </template>
 
-<style lang="scss">
-#app {
-     font-family: Avenir, Helvetica, Arial, sans-serif;
-     -webkit-font-smoothing: antialiased;
-     -moz-osx-font-smoothing: grayscale;
-     text-align: center;
-     color: #2c3e50;
-     margin-top: 60px;
-}
-</style>
+<script setup lang="ts">
+import campaignVue from './components/campaign.vue';
+import Campaign from './components/campaign.vue';
+</script>
+
--- src/components/campaign.vue
+++ src/components/campaign.vue
@@ -1,22 +1,29 @@
 <template>
-   <section class="campaign">
-      <div class="campaign_txt">
-          <h1>
-            Crocs Essential Campaign
-          </h1>
-          <p>
-            Crocs now enable customers choose what look, style or theme thier crocs would look like to fit thier with style.
-          </p>
-      </div>
-      <div class="campaign_img">
-
-      </div>
-   </section>
+     <section class="campaign">
+          <div class="campaign_txt sect_1">
+               <h1>Crocs Essential Campaign</h1>
+               <p>Crocs now enable customers choose what look, style or theme thier crocs would look like to fit thier with style.</p>
+          </div>
+          <div class="campaign_img sect_2">
+               <div class="img_cont">
+                    <img src="@/assets/croocs_1.svg" alt="" />
+               </div>
+          </div>
+     </section>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
-<style>
-
+<style lang="scss">
+.campaign {
+     @include flex(space-between, center);
+     .sect_1 {
+          background: $croc-green;
+          width: 50%;
+     }
+     .sect_2 {
+          background: $pale-orange;
+          width: 50%;
+     }
+}
 </style>
