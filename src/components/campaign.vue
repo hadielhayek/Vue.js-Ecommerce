6e051778b8a747e8584f01bf14a3b427740677b6
Jesse <akohgodwinjesse@gmail.com>
2022-07-26 20:07:38 +0100

    attrition

---
+++ src/components/campaign.vue
@@ -0,0 +1,22 @@
+<template>
+   <section class="campaign">
+      <div class="campaign_txt">
+          <h1>
+            Crocs Essential Campaign
+          </h1>
+          <p>
+            Crocs now enable customers choose what look, style or theme thier crocs would look like to fit thier with style.
+          </p>
+      </div>
+      <div class="campaign_img">
+
+      </div>
+   </section>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+
+</style>
