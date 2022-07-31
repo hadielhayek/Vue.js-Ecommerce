04297f8b5cfb2152339fa173bd45945007c91b29
Jesse <akohgodwinjesse@gmail.com>
2022-07-31 17:05:48 +0100

    yp

--- src/components/arrivals.vue
+++ src/components/arrivals.vue
@@ -10,21 +10,60 @@
                     <div class="arrivals_wrap_filters_item">Kids</div>
                     <div class="arrivals_wrap_filters_item">Top</div>
                </div>
+               <div class="arrivals_wrap_croce">
+                    <div v-for="crocs in croce" :key="crocs.id" class="arrivals_wrap_croce_item">
+                         <div class="img">
+                              <img :src="require(`@/assets/Star${crocs.id}.svg`)" alt="" />
+                         </div>
+                         <p>{{ crocs.name }}</p>
+                         <p class="_price">${{ crocs.price }}</p>
+                    </div>
+               </div>
           </div>
      </section>
 </template>
 
+<script setup lang="ts">
+import croce from "@/db/croce.json";
+</script>
+
 <style lang="scss">
 .arrivals {
      padding: 3rem 0px;
+     background: #f4fcff;
      &_wrap {
+          @include flex_col(5rem 0);
           &_filters {
                @extend %flex_r;
                &_item {
-                    font-size: 2.1em;
+                    font-size: 2.3rem;
                     border-radius: 20rem;
-                    padding: 0.5rem 1.5rem;
-                    border: 0.3rem solid black;
+                    padding: 0.5rem 1.3rem;
+                    font-family: "Athletic Medium";
+                    border: 0.2rem solid black;
+                    &:nth-child(1) {
+                         background: black;
+                         color: white;
+                    }
+               }
+          }
+          &_croce {
+               @include flex(space-around, center);
+               text-align: center;
+               &_item {
+                    @include flex_col(1.2rem 0);
+                    .img {
+                         width: 15rem;
+                         img {
+                              width: 100%;
+                         }
+                    }
+                    p {
+                         font-size: 1.7em;
+                    }
+                    ._price {
+                         color: #0f0e10a8;
+                    }
                }
           }
      }
