8588f930c994d560ec3ff7e386b7745626c5daf1
Jesse <akohgodwinjesse@gmail.com>
2022-08-01 00:27:20 +0100

    dw

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
      <Campaign />
      <Arrival />
      <Supe />
+     <Download/>
      <Cfc />
 </template>
 
 <script setup lang="ts">
+import Download from "./components/getapp.vue"
 // import landing from "./components/landing.vue"
 import Arrival from "./components/arrivals.vue";
 import Supe from "./components/superstar.vue";
---
+++ src/components/getapp.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="app_download">
+     <div class="contain_wrap app_download_wrap">
+         <h1>Get The Crocs App</h1>
+         <div class="app_download_wrap_icons">
+             <div class="img">
+                <img src="@/assets/appstore.svg" alt="">
+             </div>
+             <div class="img">
+                <img src="@/assets/playstore.svg" alt="">
+             </div>
+         </div>
+         <div class="sha256">
+             <img src="@/assets/download_1433.svg" alt="">
+         </div>
+     </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+   .app_download{
+    padding-top: 2rem;
+    text-align: center;
+      background-color: #0B0B0B;
+      &_wrap{
+        @include flex_col(1rem);
+        h1{
+            color: white;
+        }
+        &_icons{
+            @include flex(center, center){
+                gap: 0 1.8rem
+            };
+        }
+      }
+   }
+</style>
