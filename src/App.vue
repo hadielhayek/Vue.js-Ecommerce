19f0ed5b4d9186dab69df0fa3454ceea7d47beab
Jesse <akohgodwinjesse@gmail.com>
2022-07-31 16:01:24 +0100

    q

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
      <landing />
      <Campaign />
+     <Arrival />
      <Cfc />
 </template>
 
 <script setup lang="ts">
 // import landing from "./components/landing.vue"
+import Arrival from "./components/arrivals.vue";
 import Campaign from "./components/campaign.vue";
 import Cfc from "./components/funclub.vue";
 </script>
---
+++ src/components/arrivals.vue
@@ -0,0 +1,32 @@
+<template>
+     <section class="arrivals">
+          <div class="arrivals_wrap contain_wrap">
+               <div class="arrivals_wrap_filters">
+                    <div class="arrivals_wrap_filters_item">New arrival</div>
+                    <div class="arrivals_wrap_filters_item">Best Sellers</div>
+                    <div class="arrivals_wrap_filters_item">Exclusive</div>
+                    <div class="arrivals_wrap_filters_item">Men</div>
+                    <div class="arrivals_wrap_filters_item">Women</div>
+                    <div class="arrivals_wrap_filters_item">Kids</div>
+                    <div class="arrivals_wrap_filters_item">Top</div>
+               </div>
+          </div>
+     </section>
+</template>
+
+<style lang="scss">
+.arrivals {
+     padding: 3rem 0px;
+     &_wrap {
+          &_filters {
+               @extend %flex_r;
+               &_item {
+                    font-size: 2.1em;
+                    border-radius: 20rem;
+                    padding: 0.5rem 1.5rem;
+                    border: 0.3rem solid black;
+               }
+          }
+     }
+}
+</style>
