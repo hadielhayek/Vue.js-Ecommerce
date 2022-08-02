2cbde3bc0221ff4c5aff067aa3a72248e63ea6dc
Jesse <akohgodwinjesse@gmail.com>
2022-08-02 23:24:06 +0100

    working on hero

--- src/components/thirdSection.vue
+++ src/components/thirdSection.vue
@@ -13,6 +13,26 @@
                <img src="@/assets/sui6.svg" alt="">
             </div>
           </div>
+          <div class="thirdsect_txt_carousel">
+             <div class="thirdsect_txt_carousel_img">
+               <img src="@/assets/sui1.svg" alt="">
+             </div>
+             <div class="thirdsect_txt_carousel_img">
+               <img src="@/assets/sui2.svg" alt="">
+             </div>
+             <div class="thirdsect_txt_carousel_img">
+               <img src="@/assets/sui3.svg" alt="">
+             </div>
+             <div class="thirdsect_txt_carousel_img">
+               <img src="@/assets/sui4.svg" alt="">
+             </div>
+             <div class="thirdsect_txt_carousel_img">
+               <img src="@/assets/sui5.svg" alt="">
+             </div>
+             <div class="thirdsect_txt_carousel_img">
+               <img src="@/assets/sui6.svg" alt="">
+             </div>
+          </div>
           <div class="img_flex">
             <img src="@/assets/sui2.svg" alt="">
             <img src="@/assets/sui4.svg" alt="">
@@ -33,13 +53,37 @@
    padding: 4rem 0px;
    &_txt{
       @include flex_col(2rem);
+      &_carousel{
+         padding-left: 5%;
+         display: none;
+         position: relative;
+         @include media("<=tablet"){
+            display: flex;
+            white-space: nowrap;
+            gap: 0 1.3rem;
+            overflow-x: auto;
+            scroll-snap-type: x mandatory;
+            -webkit-overflow-scrolling: touch; 
+            &_img{
+               width: 22rem;
+               flex-shrink: 0;
+               scroll-snap-align: center;
+            }
+         }
+      }
       img{
          width: 14rem;
+         @include media("<=tablet"){
+            width: 100%;
+         }
       }
       h3{
          color: $red;
       }
       .abs{
+         @include media("<=tablet"){
+            display: none;
+         }
          img{
             position: absolute;
             top: 36%;
@@ -52,6 +96,9 @@
          }
       }
       .img_flex{
+                  @include media("<=tablet"){
+            display: none;
+         }
          @include flex(center, center){
             gap: 0 9rem;
          };
