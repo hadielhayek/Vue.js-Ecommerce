8daef2c0624e60f4868b8282102d8186e7811e6a
Jesse <akohgodwinjesse@gmail.com>
2022-08-02 21:59:56 +0100

    Update thirdSection.vue

---
+++ src/components/thirdSection.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="thirdsect">
+       <div class="thirdsect_txt">
+         <h1>Show Us You Love It</h1>
+         <h3>Tag @crocs to be featured here!</h3>
+       </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
