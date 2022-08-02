db25ad2ad4fe83ca071b3d505a03e49e203fc54c
Jesse <akohgodwinjesse@gmail.com>
2022-08-02 18:43:57 +0100

    jhvg

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,7 @@
 <template>
      <div class="footer">
-          <div class="footer_wrap contain_wrap">
-               <div class="footer_wrap_email">
+          <div class="footer_wrap">
+               <div class="footer_wrap_email contain_wrap">
                     <h3>We Let Friends Know About New Products</h3>
                     <div class="footer_wrap_email--div">
                          <input type="text" placeholder="Enter Your Email Address" />
@@ -23,9 +23,18 @@
                @extend %flex_r;
                padding: 2rem 0px;
                &--div {
-                  background: white;
-                  padding: 1rem;
-                  border-radius: 20px;
+                    background: white;
+                    padding: 0.7rem 2rem;
+                    border-radius: 50rem;
+                    @extend %flex_r;
+                    gap: 0 1rem;
+                    input {
+                         width: 19rem;
+                    }
+                    .btn {
+                         @include btn($croc-green, white);
+                         align-self: flex-end;
+                    }
                }
           }
      }
